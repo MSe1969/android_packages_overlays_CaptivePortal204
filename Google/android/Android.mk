0e390cf932cc8220fbe440f63340b6d21eb2dbd4
MSe1969 <mse1969@posteo.de>
2020-11-19 19:04:06 +0100

    Initial creation of Captive portal detection URL RROs
    Runtime resource overlays to overwrite the default android setup
    to connect to Google for the captive portal detection URLs generating
    the 204 code.

---
+++ Google/android/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_PACKAGE_NAME := overlay-204-android-Google
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PRIVATE_PLATFORM_APIS := true
+include $(BUILD_PACKAGE)
